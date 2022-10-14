30474f0787524b8d862106474aac8df0b6a39f39
Narika Hall <narikahall@gmail.com>
2022-10-14 12:23:31 -0500

    Added X's and O's to grid

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,18 +1,66 @@
-"use strict";
+// "use strict";
+
+
+
 
 // To load board 
 window.onload = gameboard;
 
-// const game= document.getElementById("game");
+
 
 function gameboard(){
-    board= document.getElementById("board").children;
-    for (let n = 0; n < 10; n++) {
-        board[n].setAttribute("class", "square"); 
+    const x_o_board= document.getElementById("board");
+    for (let n = 0; n < 9; n++) {
+        x_o_board.children[n].classList.add("square"); 
+        // console.log(x_o_board);
+        // console.log(x_o_board.children[n]);
+        x_o_board.children[n].addEventListener('click', clickMark, {once: true});
+        
+    }
+
+
+
+// Making a mark 
+
+//board= document.getElementById("board").children;
+
+const x_team = 'x';
+const o_team = 'o';
+const gamestate= [];
+let O
+
+console.log(x_o_board);
+
+// x_o_board.forEach(square=> {
+    // console.log("before");
+    // square.addEventListener('click', clickMark, {once: true});
+    // square.addEventListener('click', clickMark );
+//     console.log("clicked");
+// // });
+
+
+function clickMark(e) {
+    console.log("before");
+    const square = e.target
+    const X_or_O = O ? o_team : x_team
+    mark(square, X_or_O)
+    turns()
+}
+
+function mark(square, X_or_O){
+    square.classList.add(X_or_O)
+    if (X_or_O == x_team){
+        square.classList.add("X")
+        square.innerHTML = "X"
+    } else{
+        square.classList.add("O")
+        square.innerHTML= "O"
     }
 }
 
+function turns(){
+    O= !O
+}
+}
 
 
-// const list = document.getElementById("game").classList;
-// let squares= document.querySelectorAll(".square");
