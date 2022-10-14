220f5b3e6d326b30821584b85db1990282ebd76e
Narika Hall <narikahall@gmail.com>
2022-10-14 18:11:19 -0500

    Hovering was enabled

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -9,26 +9,35 @@ window.onload = gameboard;
 
 
 function gameboard(){
+    
+
     const x_o_board= document.getElementById("board");
     for (let n = 0; n < 9; n++) {
         x_o_board.children[n].classList.add("square"); 
         // console.log(x_o_board);
         // console.log(x_o_board.children[n]);
+        x_o_board.children[n].addEventListener('hover', hovering, {once: true});
         x_o_board.children[n].addEventListener('click', clickMark, {once: true});
+        // x_o_board.children[n].classList.add("hover");
+        
         
     }
 
-
+    
 
 // Making a mark 
 
 //board= document.getElementById("board").children;
+
+// x_o_board.onmouseover= function() {hovering()};
+// x_o_board.onmouseover= function() {removeHover()};
 
 const x_team = 'x';
 const o_team = 'o';
 const gamestate= [];
 let O
 
+// to remove
 console.log(x_o_board);
 
 // x_o_board.forEach(square=> {
@@ -45,6 +54,7 @@ function clickMark(e) {
     const X_or_O = O ? o_team : x_team
     mark(square, X_or_O)
     turns()
+    hovering()
 }
 
 function mark(square, X_or_O){
@@ -61,6 +71,29 @@ function mark(square, X_or_O){
 function turns(){
     O= !O
 }
+
+function hovering(e){
+    // x_o_board.classList.remove(x_team);
+    // x_o_board.classList.remove(o_team);
+    // if(!O & !X){
+    for (let n = 0; n < 9; n++) {
+        x_o_board.children[n].onmouseover= function(){
+            x_o_board.children[n].classList.add('hover');
+        };
+         x_o_board.children[n].onmouseout= function(){
+            x_o_board.children[n].classList.remove('hover');
+         }
+       
+    // } else {
+        //const square= e.target;
+        // square.classList.add("hover")
+    //     x_o_board.classList.remove("hover")
+    }
 }
-
-
+    
+// function removeHover(){
+//      x_o_board.classList.remove("hover")
+//      console.log("remved")
+// }   
+    
+}
